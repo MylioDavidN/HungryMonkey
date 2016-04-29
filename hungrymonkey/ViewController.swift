7adab03e9709aacbd1dc439e36bbad3a00d0aa61
Dung Nguyen Tien <david@Dungs-Mac-mini.local>
2016-04-29 12:26:10 -0400

    first commit

--- hungrymonkey/ViewController.swift
+++ hungrymonkey/ViewController.swift
@@ -10,9 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet var background: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // add background image
+        let bgImage = UIImage(named: "bg2.jpg")
+        let bgView = UIImageView(image: bgImage)
+        background.addSubview(bgView)
+        
+        // add monkey animation
+        var imageNames = ["monkey_2", "monkey_3"]
+        var images = [UIImage]()
+        
+        for i in 0..<imageNames.count {
+            images.append(UIImage(named: imageNames[i])!)
+        }
+        
+        imageView.animationImages = images
+        imageView.animationDuration = 1.0
+        imageView.frame = CGRect(x: 100, y: 150, width: 150, height: 150)
+        imageView.startAnimating()
+        background.addSubview(imageView)
+        
     }
 
     override func didReceiveMemoryWarning() {
